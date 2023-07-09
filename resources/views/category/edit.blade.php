2cfaf0477583d3e06f119f26a4a922958e4eac3f
Rajkumar <rajkumar04101996@gmail.com>
2023-07-09 12:01:05 +0530

    removed unwanted files

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
-<div class="container-fluid">
-</div>
-</div>
+<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8"></div>
 
     <div class="container-fluid mt--7">
         <div class="row">
